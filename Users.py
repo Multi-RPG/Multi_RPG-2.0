d8b3fea22c627820b6e3eed64606a079090a1618
w-gomes <w.gomes5521250@gmail.com>
2020-08-21 20:33:41 -0300

    Users.py

--- Users.py
+++ Users.py
@@ -12,16 +12,15 @@ class Users:
     def add_user(self):
         hm_db = Database(self.id)
         hm_db.connect()
-        return " Made account!\nYour starting :moneybag: balance: **$" + str(hm_db.insert_acct()) + "**"
+        return f" Made account!\nYour starting :moneybag: balance: **${hm_db.insert_acct()}**"
 
     def add_pet(self, pet_name):
         hm_db = Database(self.id)
         hm_db.connect()
         hm_db.insert_pet(pet_name)
         return (
-            " Adopted **" + pet_name + "**!\n\nYou can: \n1. Use **=feed** to level them up\n"
-            "2. Use **=hunt** for rewards\n"
-            "3. Use **=pet** to check their status"
+            f" Adopted **{pet_name}**!\n\nYou can: \n1. Use **=feed** to level them up\n"
+            f"2. Use **=hunt** for rewards\n3. Use **=pet** to check their status"
         )
 
     def find_user(self):
@@ -43,7 +42,7 @@ class Users:
     def donate_money(self, amnt, receiver, receiver_string):
         self.update_user_money(amnt * -1)
         receiver.update_user_money(amnt)
-        return " donated **$" + str(amnt) + "** to " + receiver_string
+        return f" donated **${amnt}** to {receiver_string}"
 
     # pass 0 to 'string' to return integer version of user's money EX: 100, default is string, EX: "**$100**"
     def get_user_money(self, string=1):
@@ -56,7 +55,7 @@ class Users:
         # if we want the full bold discord-formatted string sequence of money
         elif string == 1:
             # format money for commas and bold
-            return "**$" + str("{:,}".format(hm_db.get_money())) + "**"
+            return f"**${hm_db.get_money():,}**"
 
     # pass 0 to 'string' to return integer version of user's level EX: 3, default is string, EX: "**3**"
     def get_user_level(self, string=1):
@@ -68,7 +67,7 @@ class Users:
             return hm_db.get_level()
         # if we want the full bold discord-formatted string sequence of user level
         elif string == 1:
-            return "**" + str(hm_db.get_level()) + "**"
+            return f"**{hm_db.get_level()}**"
 
     def get_user_pet_name(self, string=1):
         hm_db = Database(self.id)
@@ -85,7 +84,7 @@ class Users:
             return hm_db.get_pet_xp()
         # if we want the full bold discord-formatted string sequence of user's pet xp
         elif string == 1:
-            return "**" + str(hm_db.get_pet_xp()) + "**"
+            return f"**{hm_db.get_pet_xp()}**"
 
     # pass 0 to 'string' to return integer version of user's pet level EX: 3, default is string, EX: "**3**"
     def get_user_pet_level(self, string=1):
@@ -97,7 +96,7 @@ class Users:
             return hm_db.get_pet_level()
         # if we want the full bold discord-formatted string sequence of user's pet level
         elif string == 1:
-            return "**" + str(hm_db.get_pet_level()) + "**"
+            return f"**{hm_db.get_pet_level()}**"
 
     def get_user_peace_status(self):
         hm_db = Database(self.id)
@@ -139,49 +138,38 @@ class Users:
                 battles_won,
                 total_winnings,
             ) = hm_db.get_battle_stats()
-            total_winnings = "{:,}".format(total_winnings)
+            total_winnings = f"{total_winnings:,}"
             user_level = hm_db.get_level()
             # format money with commas
-            user_money = "{:,}".format(hm_db.get_money())
+            user_money = f"{hm_db.get_money():,}"
 
             # add full bold discord-format to each variable
-            item_score = "**" + str(weapon_level + helmet_level + chest_level + boots_level) + "**"
+            item_score = f"**{weapon_level + helmet_level + chest_level + boots_level}**"
 
-            weapon_level = "**" + str(weapon_level) + "**"
-            helmet_level = "**" + str(helmet_level) + "**"
-            chest_level = "**" + str(chest_level) + "**"
-            boots_level = "**" + str(boots_level) + "**"
-            battles_lost = "**" + str(battles_lost) + "**"
-            battles_won = "**" + str(battles_won) + "**"
-            total_winnings = "**$" + str(total_winnings) + "**"
-            user_level = "**" + str(user_level) + "**"
-            user_money = "**$" + str(user_money) + "**"
+            weapon_level = f"**{weapon_level}**"
+            helmet_level = f"**{helmet_level}**"
+            chest_level = f"**{chest_level}**"
+            boots_level = f"**{boots_level}**"
+            battles_lost = f"**{battles_lost}**"
+            battles_won = f"**{battles_won}**"
+            total_winnings = f"**${total_winnings}**"
+            user_level = f"**{user_level}**"
+            user_money = f"**${user_money}**"
 
             # have to insert encode \u200B for spaces when using discord encoding
             return (
-                "\n**ACCOUNT:**"
-                "\n:chart_with_upwards_trend: Level: \u200B \u200B \u200B \u200B \u200B \u200B"
-                + user_level
-                + "\n:moneybag: Money: \u200B \u200B"
-                + user_money
-                + "\n\n**GEAR:**"
-                "\n<:weapon1:532252764097740861> Weapon: \u200B \u200B"
-                + weapon_level
-                + "\n<:helmet2:532252796255469588> Helmet: \u200B \u200B \u200B \u200B"
-                + helmet_level
-                + "\n<:chest5:532255708679503873> Chest: \u200B \u200B \u200B \u200B \u200B \u200B \u200B"
-                + chest_level
-                + "\n<:boots1:532252814953676807> Boots: \u200B \u200B \u200B \u200B \u200B \u200B \u200B"
-                + boots_level
-                + "\n__Total__:\u200B \u200B \u200B"
-                + item_score
-                + "\n\n**TOURNAMENT RECORDS:**"
-                "\n:crossed_swords:\u200B \u200B lost: \u200B \u200B"
-                + battles_lost
-                + "\n:crossed_swords:\u200B \u200B won: "
-                + battles_won
-                + "\n__Total winnings__: "
-                + total_winnings
+                f"\n**ACCOUNT:**"
+                f"\n:chart_with_upwards_trend: Level: \u200B \u200B \u200B \u200B \u200B \u200B"
+                f"{user_level}\n:moneybag: Money: \u200B \u200B {user_money}"
+                f"\n\n**GEAR:**\n<:weapon1:532252764097740861> Weapon: \u200B \u200B {weapon_level}"
+                f"\n<:helmet2:532252796255469588> Helmet: \u200B \u200B \u200B \u200B {helmet_level}"
+                f"\n<:chest5:532255708679503873> Chest: \u200B \u200B \u200B \u200B \u200B \u200B \u200B {chest_level}"
+                f"\n<:boots1:532252814953676807> Boots: \u200B \u200B \u200B \u200B \u200B \u200B \u200B {boots_level}"
+                f"\n__Total__:\u200B \u200B \u200B {item_score}"
+                f"\n\n**TOURNAMENT RECORDS:**"
+                f"\n:crossed_swords:\u200B \u200B lost: \u200B \u200B {battles_lost}"
+                f"\n:crossed_swords:\u200B \u200B won: \u200B {battles_won}"
+                f"\n__Total winnings__: {total_winnings}"
             )
 
     def get_user_ticket_status(self):
@@ -200,15 +188,14 @@ class Users:
     def update_user_money(self, amount):
         hm_db = Database(self.id)
         hm_db.connect()
-        return "Your new account balance: **$" + str("{:,}".format(hm_db.update_money(amount))) + "**"
+        return f"Your new account balance: **${hm_db.update_money(amount):,}**"
 
     def update_user_level(self):
         hm_db = Database(self.id)
         hm_db.connect()
         return (
-            "\n<:worrysign10:531221748964786188> New level: **"
-            + str(hm_db.update_level())
-            + "** <:worrysign10:531221748964786188>"
+            f"\n<:worrysign10:531221748964786188> New level: "
+            f"**{hm_db.update_level()}** <:worrysign10:531221748964786188>"
         )
 
     def update_user_pet_xp(self):
@@ -242,13 +229,13 @@ class Users:
         hm_db.connect()
 
         if gear_type == "weapon":
-            return " Your new total item score: **" + str(hm_db.update_battle_weapon(level)) + "**"
+            return f" Your new total item score: **{hm_db.update_battle_weapon(level)}**"
         elif gear_type == "helmet":
-            return " Your new total item score: **" + str(hm_db.update_battle_helmet(level)) + "**"
+            return f" Your new total item score: **{hm_db.update_battle_helmet(level)}**"
         elif gear_type == "chest":
-            return " Your new total item score: **" + str(hm_db.update_battle_chest(level)) + "**"
+            return f" Your new total item score: **{hm_db.update_battle_chest(level)}**"
         elif gear_type == "boots":
-            return " Your new total item score: **" + str(hm_db.update_battle_boots(level)) + "**"
+            return f" Your new total item score: **{hm_db.update_battle_boots(level)}**"
 
     def update_user_tourney_server_id(self, server_name, server_id):
         hm_db = Database(self.id)
@@ -256,26 +243,19 @@ class Users:
 
         hm_db.update_tourney_server_id(server_id)
         return (
-            ":crossed_swords: Registered for **"
-            + server_name
-            + "** daily FFA tournament! :crossed_swords:"
-            + "\nResults will be live by :alarm_clock: **7 AM PST!**"
+            f":crossed_swords: Registered for **{server_name}** daily FFA tournament! :crossed_swords:"
+            f"\nResults will be live by :alarm_clock: **7 AM PST!**"
         )
 
     def update_user_records(self, battles_lost, battles_won, total_winnings):
         hm_db = Database(self.id)
         hm_db.connect()
 
-        return (
-            " Your new battle records: **"
-            + str(hm_db.update_battle_records(battles_lost, battles_won, total_winnings))
-            + "**"
-        )
+        return f" Your new battle records: **{hm_db.update_battle_records(battles_lost, battles_won, total_winnings)}**"
 
     def update_user_lottery_guess(self, ticket_guess, ticket_active):
         hm_db = Database(self.id)
         hm_db.connect()
 
         hm_db.update_lottery_guess(ticket_guess, ticket_active)
-        return (f"Thanks! Processed ticket ID: **{self.id}**\n"
-                f"Results will be live by :alarm_clock: **7 AM PST!**")
+        return f"Thanks! Processed ticket ID: **{self.id}**\n" f"Results will be live by :alarm_clock: **7 AM PST!**"
